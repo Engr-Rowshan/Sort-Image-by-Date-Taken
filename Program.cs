911412268bf7f6e3324deddf212de37181d58f86
Engr. Syed Rowshan Ali <engr.rowshan@gmail.com>
2019-09-01 15:28:13 +0600

    Comment Added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -12,15 +10,18 @@ namespace Sort_Image_by_Date_Taked
 {
     class Program
     {
-        static public Queue<string> jpgPathQuery = new Queue<string>();
-        static public int count;
-        static public string DestinationPath;
-        static public string date, time;
+        static public Queue<string> jpgPathQuery = new Queue<string>(); //Queue to hold image paths
+        static public int count;                                        //Count total jpg,jpeg, png
+        static public string DestinationPath;                           //Destinationp path for image
+        static public string dirName, fileName;                                //global variable needed to store directory name and file na,e
 
         static int Main(string[] args)
         {
-            //Just make a queue to load all files in it
-            DestinationPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + Path.DirectorySeparatorChar + "export"; 
+
+            //Making the destination path
+            DestinationPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + Path.DirectorySeparatorChar + "export";
+
+            //First arg should be src directory path
             if (args.Length > 0)
             {
                 string path = args[0];
@@ -28,18 +29,19 @@ namespace Sort_Image_by_Date_Taked
                 {
                     try
                     {
-                        FileAttributes attr = File.GetAttributes(path);
+                        FileAttributes attr = File.GetAttributes(path);             //Make sure it is directory
                         if (attr.HasFlag(FileAttributes.Directory)) {
-                            Console.WriteLine("Path is a Directory: " + path);
-                            DirSearch(path);
-                            Console.WriteLine("Total enqueued: " + count.ToString());
 
+                            DirSearch(path);                                        //Start Recursive Search //TODO: Make it multi threaded
+
+                            Console.WriteLine("Total enqueued: " + count.ToString());   //Outout Total image count
+
+                            //Start Denquing and Moding
                             int countx = jpgPathQuery.Count;
                             for(int i =0; i < countx; i++)
                             {
                                 StartSortingJpegs();
                             }
-
                         }
                         else
                         {
@@ -64,43 +66,43 @@ namespace Sort_Image_by_Date_Taked
 
         static void StartSortingJpegs()
         {
-            string image = jpgPathQuery.Dequeue();
-            string ext = Path.GetExtension(image).ToLower();
+            string image = jpgPathQuery.Dequeue();                  //Pull fisrt image
+            string ext = Path.GetExtension(image).ToLower();        //Get the extention
+
+            //using bitmap to read propertyItems
             using (Bitmap b = new Bitmap(image))
             {
                 PropertyItem[] propItems = b.PropertyItems;
 
-                ASCIIEncoding encodings = new ASCIIEncoding();
+                ASCIIEncoding encodings = new ASCIIEncoding();      //Enconding to human readable code
 
                 foreach (PropertyItem item in propItems)
                 {
-                    if(item.Id == 0x9003)
+                    if(item.Id == 0x9003)                           //Property ID 0x9003 holds datetime of image taken value
                     {
-                    Console.WriteLine("0x" + item.Id.ToString("x"));
-                    try
-                    {
-                        string DT = encodings.GetString(item.Value);
-                        Console.WriteLine("The Date & Time is " + DT.Replace(":","-"));
+                        try
+                        {
+                            string DT = encodings.GetString(item.Value);
+                            Console.WriteLine("The Date & Time is " + DT.Replace(":","-"));
 
 
-                            string[] tmp = DT.Replace(":", "-").Split(' ');
-                            date = tmp[0];
-                            time = tmp[1].Substring(0,tmp[1].Length - 1);
+                                string[] tmp = DT.Replace(":", "-").Split(' ');
+                                dirName = tmp[0];
+                                fileName = tmp[1].Substring(0,tmp[1].Length - 1);
 
-                            //Move(image, DestinationPath, date, time, ext);
 
-                    }
-                    catch(EncoderFallbackException e)
-                    {
-                        Console.WriteLine("No Date Time");
-                    }
-                    Console.WriteLine("");
+                        }
+                        catch(EncoderFallbackException e)
+                        {
+                            Console.WriteLine("No Date Time");
+                        }
+                        Console.WriteLine("");
                     }
                    
                 }
             }
-
-            Move(image, DestinationPath, date, time, ext);
+            //This move command is called here beause inside using block the file is locked with a handler
+            Move(image, DestinationPath, dirName, fileName, ext);
         }
         static void Move(string srcPath, string destPath, string dir, string fileName, string ext)
         {
